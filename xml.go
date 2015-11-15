f08ac1da62c9c3c2d7725a57af4eef1f3c2e15fe
Charles Banning <clbanning@tamgroupinc.com>
2015-11-15 06:47:45 -0600

    patch mapToXmlIndent() per clbanning/mxj

--- xml.go
+++ xml.go
@@ -12,6 +12,7 @@ import (
 	"encoding/xml"
 	"errors"
 	"fmt"
+	"sort"
 )
 
 
@@ -118,9 +119,11 @@ func (p *pretty) Outdent() {
 
 // where the work actually happens
 // returns an error if an attribute is not atomic
+// patched with new version in github.com/clbanning/mxj - 2015.11.15
 func mapToXmlIndent(doIndent bool, s *string, key string, value interface{}, pp *pretty) error {
 	var endTag bool
 	var isSimple bool
+	var elen int
 	p := &pretty{pp.indent, pp.cnt, pp.padding, pp.mapDepth, pp.start}
 
 	switch value.(type) {
@@ -136,20 +139,32 @@ func mapToXmlIndent(doIndent bool, s *string, key string, value interface{}, pp 
 		lenvv := len(vv)
 		// scan out attributes - keys have prepended hyphen, '-'
 		var cntAttr int
+		attrlist := make([][2]string, len(vv))
+		var n int
 		for k, v := range vv {
 			if k[:1] == "-" {
+				cntAttr++
 				switch v.(type) {
 				case string, float64, bool, int, int32, int64, float32:
-					*s += ` ` + k[1:] + `="` + fmt.Sprintf("%v", v) + `"`
-					cntAttr++
-				case []byte: // allow standard xml pkg []byte transform, as below
-					*s += ` ` + k[1:] + `="` + fmt.Sprintf("%v", string(v.([]byte))) + `"`
-					cntAttr++
+					attrlist[n][0] = k[1:]
+					attrlist[n][1] = fmt.Sprintf("%v", v)
+					n++
+				case []byte:
+					attrlist[n][0] = k[1:]
+					attrlist[n][1] = fmt.Sprintf("%v", string(v.([]byte)))
 				default:
 					return fmt.Errorf("invalid attribute value for: %s", k)
 				}
 			}
 		}
+		if cntAttr > 0 {
+			attrlist = attrlist[:n]
+			sort.Sort(attrList(attrlist))
+			for _, v := range attrlist {
+				*s += ` ` + v[0] + `="` + v[1] + `"`
+			}
+		}
+
 		// only attributes?
 		if cntAttr == lenvv {
 			break
@@ -161,6 +176,8 @@ func mapToXmlIndent(doIndent bool, s *string, key string, value interface{}, pp 
 			}
 			*s += ">" + fmt.Sprintf("%v", v)
 			endTag = true
+			elen = 1
+			isSimple = true
 			break
 		}
 		// close tag with possible attributes
@@ -170,12 +187,22 @@ func mapToXmlIndent(doIndent bool, s *string, key string, value interface{}, pp 
 		}
 		// something more complex
 		p.mapDepth++
-		var i int
+		// extract the map k:v pairs and sort on key
+		elemlist := make([][2]interface{}, len(vv))
+		n = 0
 		for k, v := range vv {
 			if k[:1] == "-" {
 				continue
 			}
-			switch v.(type) {
+			elemlist[n][0] = k
+			elemlist[n][1] = v
+			n++
+		}
+		elemlist = elemlist[:n]
+		sort.Sort(elemList(elemlist))
+		var i int
+		for _, v := range elemlist {
+			switch v[1].(type) {
 			case []interface{}:
 			default:
 				if i == 0 && doIndent {
@@ -183,8 +210,8 @@ func mapToXmlIndent(doIndent bool, s *string, key string, value interface{}, pp 
 				}
 			}
 			i++
-			mapToXmlIndent(doIndent, s, k, v, p)
-			switch v.(type) {
+			mapToXmlIndent(doIndent, s, v[0].(string), v[1], p)
+			switch v[1].(type) {
 			case []interface{}: // handled in []interface{} case
 			default:
 				if doIndent {
@@ -195,6 +222,7 @@ func mapToXmlIndent(doIndent bool, s *string, key string, value interface{}, pp 
 		}
 		p.mapDepth--
 		endTag = true
+		elen = 1 // we do have some content ...
 	case []interface{}:
 		for _, v := range value.([]interface{}) {
 			if doIndent {
@@ -211,12 +239,21 @@ func mapToXmlIndent(doIndent bool, s *string, key string, value interface{}, pp 
 		*s += "<" + key
 		break
 	default: // handle anything - even goofy stuff
+		elen = 0
 		switch value.(type) {
 		case string, float64, bool, int, int32, int64, float32:
-			*s += ">" + fmt.Sprintf("%v", value)
+			v := fmt.Sprintf("%v", value)
+			elen = len(v)
+			if elen > 0 {
+				*s += ">" + v
+			}
 		case []byte: // NOTE: byte is just an alias for uint8
 			// similar to how xml.Marshal handles []byte structure members
-			*s += ">" + string(value.([]byte))
+			v := string(value.([]byte))
+			elen = len(v)
+			if elen > 0 {
+				*s += ">" + v
+			}
 		default:
 			var v []byte
 			var err error
@@ -228,25 +265,31 @@ func mapToXmlIndent(doIndent bool, s *string, key string, value interface{}, pp 
 			if err != nil {
 				*s += ">UNKNOWN"
 			} else {
-				*s += string(v)
+				elen = len(v)
+				if elen > 0 {
+					*s += string(v)
+				}
 			}
 		}
 		isSimple = true
 		endTag = true
 	}
-
 	if endTag {
 		if doIndent {
 			if !isSimple {
-//				if p.mapDepth == 0 {
-//					p.Outdent()
-//				}
 				*s += p.padding
 			}
 		}
 		switch value.(type) {
 		case map[string]interface{}, []byte, string, float64, bool, int, int32, int64, float32:
-			*s += `</` + key + ">"
+			if elen > 0 || UseGoEmptyElementSyntax {
+				if elen == 0 {
+					*s += ">"
+				}
+				*s += `</` + key + ">"
+			} else {
+				*s += `/>`
+			}
 		}
 	} else if UseGoEmptyElementSyntax {
 		*s += "></" + key + ">"
@@ -261,4 +304,40 @@ func mapToXmlIndent(doIndent bool, s *string, key string, value interface{}, pp 
 	}
 
 	return nil
+}
+
+// ============================ sort interface implementation =================
+
+type attrList [][2]string
+
+func (a attrList) Len() int {
+	return len(a)
+}
+
+func (a attrList) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a attrList) Less(i, j int) bool {
+	if a[i][0] > a[j][0] {
+		return false
+	}
+	return true
+}
+
+type elemList [][2]interface{}
+
+func (e elemList) Len() int {
+	return len(e)
+}
+
+func (e elemList) Swap(i, j int) {
+	e[i], e[j] = e[j], e[i]
+}
+
+func (e elemList) Less(i, j int) bool {
+	if e[i][0].(string) > e[j][0].(string) {
+		return false
+	}
+	return true
 }
